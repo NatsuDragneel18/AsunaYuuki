54548bf7f2503261847941b32043b8b341dd95a1
Yoga Pranata <45356875+zYxDevs@users.noreply.github.com>
2021-06-30 20:52:19 +0700

    Update __init__.py

--- LaylaRobot/__init__.py
+++ LaylaRobot/__init__.py
@@ -111,7 +111,7 @@ else:
 
     JOIN_LOGGER = Config.JOIN_LOGGER
     OWNER_USERNAME = Config.OWNER_USERNAME
-    ALLOW_CHATS = Config.ALLOW_CHATS
+    ALLOW_CHATS = int(Config.ALLOW_CHATS)
     try:
         DRAGONS = set(int(x) for x in Config.DRAGONS or [])
         DEV_USERS = set(int(x) for x in Config.DEV_USERS or [])
